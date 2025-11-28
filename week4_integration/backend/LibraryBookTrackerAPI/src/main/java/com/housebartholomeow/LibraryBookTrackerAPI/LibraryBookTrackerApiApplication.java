89d31da0ba7a2c7285d279758da38c812fa73dd6
Shadiq Aryasatya <skulltrooperfortnite2017@gmail.com>
2025-11-28 15:25:47 +0700

    added main application file

---
+++ week4_integration/backend/LibraryBookTrackerAPI/src/main/java/com/housebartholomeow/LibraryBookTrackerAPI/LibraryBookTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.housebartholomeow.LibraryBookTrackerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBookTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBookTrackerApiApplication.class, args);
+	}
+
+}
